5a34bb97c0a983392b5289dee65f7b8e305f94a3
Rajeshwari33 <rajeshwari@adventbizsolutions.com>
2021-01-14 12:59:52 +0530

    IO Engine Model

--- Components/Web/api/adbizIO/adbizIO/settings.py
+++ Components/Web/api/adbizIO/adbizIO/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adbizIOEngine',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'adbizIO.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'adbiz_io',
+        'USER': 'root',
+        'PASSWORD': 'admin@1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
